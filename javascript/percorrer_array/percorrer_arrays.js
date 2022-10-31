b3640399e679c10346e83bdf8e26aabf88af378a
Laura Vital Cacique <93387750+lauravitalc@users.noreply.github.com>
2022-10-31 19:51:13 -0300

    Mais Array e Loops

---
+++ javascript/percorrer_array/percorrer_arrays.js
@@ -0,0 +1,16 @@
+var lista = Array();
+
+lista[0] = 'Chandler';
+lista[1] = 'Ross'
+lista[2] = 'Joey'
+lista[3] = 'Monica'
+lista[4] = 'Phoebe'
+lista[5] = 'Rachel'
+
+y = 0
+
+while (y < lista.length) {
+    console.log(lista[y])
+    y++
+}
+
