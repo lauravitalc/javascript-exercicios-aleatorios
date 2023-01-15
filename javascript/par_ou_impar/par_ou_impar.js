83b6c3058a6c47f1bdfb38864e7c6a289a8c5571
Laura Vital Cacique <93387750+lauravitalc@users.noreply.github.com>
2023-01-15 10:43:12 -0300

    Leetcode - 1 and 283

--- javascript/par_ou_impar/par_ou_impar.js
+++ javascript/par_ou_impar/par_ou_impar.js
@@ -1,9 +1,16 @@
-function numero(){
-    var numero = parseInt(document.getElementById("n1").value);
-    var resultado = document.getElementById("resultado");
-    if(numero % 2 == 0){
-        resultado.innerHTML = `O número ${numero} é par`;
-    } else {
-        resultado.innerHTML = `O número ${numero} é impar`;
-    }
+// function numero(){
+//     var numero = parseInt(document.getElementById("n1").value);
+//     var resultado = document.getElementById("resultado");
+//     if(numero % 2 == 0){
+//         resultado.innerHTML = `O número ${numero} é par`;
+//     } else {
+//         resultado.innerHTML = `O número ${numero} é impar`;
+//     }
+// }
+
+function parOuImpar(n){
+    (n % 2 == 0) ? console.log('O número é par') : console.log('o número é impar');
 }
+
+parOuImpar(45)
+parOuImpar(44);
